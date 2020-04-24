588127caaa590bc86ae83da8e7faf04c11504ce5
nroque <natalia.e.roque@gmail.com>
2020-04-24 13:54:59 -0400

    Updates

---
+++ precache-manifest.bbc878a7e496456270cd9da5011b04ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3b1ed789f82881b738845304ab27f96",
+    "url": "/tabs/index.html"
+  },
+  {
+    "revision": "eecffae62e97a099c17c",
+    "url": "/tabs/static/js/2.01f344ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tabs/static/js/2.01f344ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2e34b170b44993b508b",
+    "url": "/tabs/static/js/main.78edb207.chunk.js"
+  },
+  {
+    "revision": "13212215235fae8f1fd5",
+    "url": "/tabs/static/js/runtime-main.39b89dc9.js"
+  },
+  {
+    "revision": "9e55b3ed1101a7250ce6da351d052064",
+    "url": "/tabs/static/media/girl.9e55b3ed.jpg"
+  }
+]);
